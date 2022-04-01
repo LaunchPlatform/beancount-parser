f2f5c65764847b95ed0d06957b7b650277a67ff2
Fang-Pen Lin <hello@fangpenlin.com>
2022-03-31 17:06:07 -0700

    Add more syntax

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,5 +3,5 @@ from beancount_parser.parser import make_parser
 
 def test_parse():
     parser = make_parser()
-    result = parser.parse("2022-03-31")
+    result = parser.parse('2022-03-31 * "Foobar"\n Assets:MyBank 12.34')
     assert result
