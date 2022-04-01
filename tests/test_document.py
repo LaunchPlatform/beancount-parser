7a4e2ff36e7b3d8f8ab2a75ab8b7c4658a06907d
Fang-Pen Lin <hello@fangpenlin.com>
2022-03-31 23:31:02 -0700

    Add document

---
+++ tests/test_document.py
@@ -0,0 +1,40 @@
+import pytest
+from lark import Lark
+from lark.exceptions import UnexpectedInput
+
+from beancount_parser.parser import GRAMMAR_FOLDER
+
+
+@pytest.fixture
+def document_parser() -> Lark:
+    return Lark(
+        """
+    start: document
+    %import .document.document
+    %ignore " "
+    """,
+        import_paths=[GRAMMAR_FOLDER],
+    )
+
+
+@pytest.mark.parametrize(
+    "text",
+    [
+        '2022-03-31 document Assets "/path/to/the/file.pdf"',
+        '2022-03-31 document Assets "/path/to/the/file.pdf" ; this is a comment',
+    ],
+)
+def test_parse_document(document_parser: Lark, text: str):
+    document_parser.parse(text)
+
+
+@pytest.mark.parametrize(
+    "text",
+    [
+        'document Assets "this account looks good"',
+        "2022-03-31 document Assets",
+    ],
+)
+def test_parse_bad_document(document_parser: Lark, text: str):
+    with pytest.raises(UnexpectedInput):
+        document_parser.parse(text)
