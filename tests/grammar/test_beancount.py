3a60162fe73fb6da1ea0e9c4e95795fbcc707b9e
Fang-Pen Lin <hello@fangpenlin.com>
2022-04-01 23:11:46 -0700

    xxx

--- tests/grammar/test_beancount.py
+++ tests/grammar/test_beancount.py
@@ -16,11 +16,44 @@ def parser() -> Lark:
     [
         dedent(
             """\
+    1970-01-01 commodity USD
     1970-01-01 open Assets:MyBank USD
-        foo: "bar"
-    1970-01-01 balance Assets:MyBank 123 USD
     
+    option "foo" "bar"
+    include "2022.bean"
+    plugin "beancount.module"
     
+    """
+        ),
+        dedent(
+            """\
+    2022-03-31 * "Foobar"
+        Assets:MyBank 12.34 USD ; this is fine
+    """
+        ),
+        dedent(
+            """\
+    1970-01-01 commodity USD
+    1970-01-01 open Assets:MyBank USD
+    1970-01-01 note Assets:MyBank "this is my first bank account"
+    1970-01-01 open Assets:My2ndBank USD,BTC
+    1970-01-01 close Assets:My2ndBank
+    1970-01-01 commodity USD
+    
+       ; comment
+    ; comment
+    1970-01-01 event "job" "working from home"
+
+    2022-03-31
+        Assets:MyBank 12.34 USD
+    
+    2022-03-31 * "Foobar"
+        document: "foobar.pdf" ; my doc
+        document-2: "egg-spam.pdf"
+        Assets:MyBank 12.34 USD ; this is fine
+            document: "invoice.pdf"
+            source: "invoice.pdf"
+
     """
         ),
     ],
