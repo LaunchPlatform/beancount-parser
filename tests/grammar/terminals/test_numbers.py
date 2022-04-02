d7e0ffcf1e728d0fcdd2e99ad3dd96f347461bb0
Fang-Pen Lin <hello@fangpenlin.com>
2022-04-02 01:43:39 -0700

    Fix number doesn't support comma issue

--- tests/grammar/terminals/test_numbers.py
+++ tests/grammar/terminals/test_numbers.py
@@ -18,9 +18,14 @@ def signed_number_parser(make_parser: typing.Callable) -> Lark:
 @pytest.mark.parametrize(
     "text",
     [
-        "0",
+        "1",
+        "12",
+        "123",
         "1234",
         "4578",
+        "4,578",
+        "4,578.1234",
+        "1,123,578.1234",
         "12.34",
         ".34",
         "0.0",
@@ -34,6 +39,9 @@ def test_parse_number(number_parser: Lark, text: str):
     "text",
     [
         "-0",
+        "1000,000",
+        "1000,00",
+        "1,0000",
         "+1234",
         "-5.67",
         "4578.123.45",
