a69712cf7e68e6e46451c2755b17de6fdb148e62
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-05 17:31:53 -0700

    Add helper

---
+++ beancount_parser/data_types.py
@@ -0,0 +1,66 @@
+import enum
+import typing
+
+from lark import Tree
+
+
+@enum.unique
+class EntryType(enum.StrEnum):
+    # Date directives
+    OPEN = "OPEN"
+    CLOSE = "CLOSE"
+    BALANCE = "BALANCE"
+    EVENT = "EVENT"
+    COMMODITY = "COMMODITY"
+    DOCUMENT = "DOCUMENT"
+    PRICE = "PRICE"
+    NOTE = "NOTE"
+    PAD = "PAD"
+    CUSTOM = "CUSTOM"
+    TXN = "TXN"
+    # Simple directives
+    OPTION = "OPTION"
+    INCLUDE = "INCLUDE"
+    PLUGIN = "PLUGIN"
+    # Other
+    COMMENTS = "COMMENTS"
+    SECTION_HEADER = "SECTION_HEADER"
+
+
+DATE_DIRECTIVE_ENTRY_TYPES = {
+    "open": EntryType.OPEN,
+    "close": EntryType.CLOSE,
+    "balance": EntryType.BALANCE,
+    "event": EntryType.EVENT,
+    "commodity": EntryType.COMMODITY,
+    "document": EntryType.DOCUMENT,
+    "price": EntryType.PRICE,
+    "note": EntryType.NOTE,
+    "pad": EntryType.PAD,
+    "custom": EntryType.CUSTOM,
+    "txn": EntryType.TXN,
+}
+SIMPLE_DIRECTIVE_ENTRY_TYPES = {
+    "option": EntryType.OPTION,
+    "include": EntryType.INCLUDE,
+    "plugin": EntryType.PLUGIN,
+}
+
+
+class Metadata(typing.NamedTuple):
+    comments: list[Tree]
+    statement: Tree
+
+
+class Posting(typing.NamedTuple):
+    comments: list[Tree]
+    statement: Tree
+    metadata: list[Metadata]
+
+
+class Entry(typing.NamedTuple):
+    type: EntryType
+    comments: typing.List[Tree]
+    statement: Tree | None
+    metadata: typing.List[Metadata]
+    postings: typing.List[Posting]
---
+++ beancount_parser/helpers.py
@@ -0,0 +1,75 @@
+from lark import Token
+from lark import Tree
+
+from .data_types import DATE_DIRECTIVE_ENTRY_TYPES
+from .data_types import Entry
+from .data_types import EntryType
+from .data_types import Metadata
+from .data_types import Posting
+from .data_types import SIMPLE_DIRECTIVE_ENTRY_TYPES
+
+
+def get_entry_type(statement: Tree) -> EntryType:
+    first_child: Tree = statement.children[0]
+    if first_child.data == "date_directive":
+        return DATE_DIRECTIVE_ENTRY_TYPES[first_child.children[0].data.value]
+    elif first_child.data == "simple_directive":
+        return SIMPLE_DIRECTIVE_ENTRY_TYPES[first_child.children[0].data.value]
+    else:
+        raise ValueError(f"Unexpected first child type {first_child.data}")
+
+
+def collect_entries(tree: Tree) -> tuple[list[Tree], list[Entry]]:
+    entries: list[Entry] = []
+    comments: list[Tree] = []
+    for statement in tree.children:
+        if statement is None:
+            continue
+        if statement.data != "statement":
+            raise ValueError("Expected statement here")
+        first_child = statement.children[0]
+        if isinstance(first_child, Token):
+            if first_child.type == "COMMENT":
+                comments.append(statement)
+            elif first_child.type == "SECTION_HEADER":
+                entry = Entry(
+                    type=EntryType.SECTION_HEADER,
+                    comments=comments,
+                    statement=statement,
+                    metadata=[],
+                    postings=[],
+                )
+                entries.append(entry)
+                comments = []
+            else:
+                raise ValueError(f"Unexpected token {first_child.type}")
+        else:
+            if first_child.data == "posting":
+                last_entry = entries[-1]
+                if last_entry.type != EntryType.TXN:
+                    raise ValueError("Transaction expected")
+                last_entry.postings.append(
+                    Posting(comments=comments, statement=statement, metadata=[])
+                )
+                comments = []
+                continue
+            elif first_child.data == "metadata_item":
+                last_entry = entries[-1]
+                metadata = Metadata(comments=comments, statement=statement)
+                if last_entry.postings:
+                    last_posting: Posting = last_entry.postings[-1]
+                    last_posting.metadata.append(metadata)
+                else:
+                    last_entry.metadata.append(metadata)
+                comments = []
+                continue
+            entry = Entry(
+                type=get_entry_type(statement),
+                comments=comments,
+                statement=statement,
+                metadata=[],
+                postings=[],
+            )
+            entries.append(entry)
+            comments = []
+    return comments, entries
