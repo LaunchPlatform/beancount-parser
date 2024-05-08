16d4c8ffc744e4bd06e854c7b4979aa4e8802ca3
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-08 11:25:17 -0700

    Add more log msgs

--- beancount_parser/parser.py
+++ beancount_parser/parser.py
@@ -1,5 +1,6 @@
 import glob
 import json
+import logging
 import pathlib
 import typing
 
@@ -16,7 +17,7 @@ def make_parser(**options: typing.Any) -> Lark:
         return Lark(grammar=fo, **(default_options | options))
 
 
-def extract_includes(tree: Tree):
+def extract_includes(tree: Tree) -> typing.Generator[tuple[str, int], None, None]:
     """Extract include statements from the root tree"""
     if tree.data != "start":
         raise ValueError("Expected start")
@@ -33,16 +34,17 @@ def extract_includes(tree: Tree):
         include = first_child.children[0]
         if include.data != "include":
             continue
-        yield json.loads(include.children[0].value)
+        yield json.loads(include.children[0].value), first_child.meta.line
 
 
 def traverse(
     parser: Lark, bean_file: pathlib.Path, root_dir: pathlib.Path | None = None
 ) -> typing.Generator[tuple[pathlib.Path, Tree], None, None]:
     """Traverse a given bean file and follow all its includes, yield (path, parsed_tree) tuples"""
+    logger = logging.getLogger(__name__)
     visited_bean_files: set[pathlib.Path] = set()
 
-    if root_dir is None:
+    if root_dir is not None:
         root_dir = bean_file.parent.absolute()
     pending_files = [bean_file.absolute()]
 
@@ -52,11 +54,22 @@ def traverse(
         tree = parser.parse(current_file.read_text())
         yield current_file, tree
         includes = extract_includes(tree)
-        for include in includes:
+        for include, lineno in includes:
+            logger.info(
+                "Process include at %s:%s with path value %s",
+                current_file,
+                lineno,
+                include,
+            )
             target_file = current_file.parent / include
             for matched_file in glob.glob(str(target_file)):
                 matched_file = pathlib.Path(matched_file).resolve().absolute()
                 if root_dir not in matched_file.parents:
+                    logger.warning(
+                        "Matched file %s is not a sub-path of root %s, ignored",
+                        matched_file,
+                        root_dir,
+                    )
                     # ensure include cannot go above the root folder, to avoid any potential security risk
                     continue
                 if matched_file in visited_bean_files:
